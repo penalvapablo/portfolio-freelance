7dd5b0cb38ab53e37fa0452210339773f779e2a5
Pablo Penalva <penalva.pablo@gmail.com>
2023-08-31 17:08:11 -0300

    github linkedin links

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faGithubAlt } from '@fortawesome/free-brands-svg-icons';
+import { faGithubSquare } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -25,6 +30,29 @@ const SocialLinks = () => {
         className=" ">
         <FontAwesomeIcon
           icon={faWhatsapp}
+          className="h-11 w-11 duration-300 hover:text-secondary"
+        />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/pablo-penalva/"
+        rel="noreferrer"
+        target="_blank"
+        aria-label="link to linkedin"
+        className=" ">
+        <FontAwesomeIcon
+          icon={faLinkedin}
+          className="h-11 w-11 duration-300 hover:text-secondary"
+        />
+      </a>
+
+      <a
+        href="https://github.com/penalvapablo"
+        rel="noreferrer"
+        target="_blank"
+        aria-label="link to github"
+        className=" ">
+        <FontAwesomeIcon
+          icon={faGithub}
           className="h-11 w-11 duration-300 hover:text-secondary"
         />
       </a>
