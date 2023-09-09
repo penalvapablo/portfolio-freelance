a17898c2ba46b8a151c91d99d5c0dd9bf6b557ef
Pablo Penalva <penalva.pablo@gmail.com>
2023-09-09 14:52:16 -0300

    new images

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -11,28 +11,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const SocialLinks = () => {
   return (
     <div
-      className="mt-3 flex justify-center gap-6 text-white opacity-0 xl:justify-start"
+      className="mt-10 flex justify-center gap-6 text-white "
       id="social-links">
-      <a
-        href="mailto:penalva.pablo@gmail.com"
-        aria-label="link to email"
-        className="h-8">
-        <FontAwesomeIcon
-          icon={faEnvelope}
-          className="h-11 duration-300 hover:text-secondary"
-        />
-      </a>
-      <a
-        href="https://api.whatsapp.com/send?phone=5491154192392"
-        rel="noreferrer"
-        target="_blank"
-        aria-label="link to whatsapp"
-        className=" ">
-        <FontAwesomeIcon
-          icon={faWhatsapp}
-          className="h-11 w-11 duration-300 hover:text-secondary"
-        />
-      </a>
       <a
         href="https://www.linkedin.com/in/pablo-penalva/"
         rel="noreferrer"
@@ -54,6 +34,27 @@ const SocialLinks = () => {
         <FontAwesomeIcon
           icon={faGithub}
           className="h-11 w-11 duration-300 hover:text-secondary"
+        />
+      </a>
+
+      <a
+        href="https://api.whatsapp.com/send?phone=5491154192392"
+        rel="noreferrer"
+        target="_blank"
+        aria-label="link to whatsapp"
+        className=" ">
+        <FontAwesomeIcon
+          icon={faWhatsapp}
+          className="h-11 w-11 duration-300 hover:text-secondary"
+        />
+      </a>
+      <a
+        href="mailto:penalva.pablo@gmail.com"
+        aria-label="link to email"
+        className="h-8">
+        <FontAwesomeIcon
+          icon={faEnvelope}
+          className="h-11 duration-300 hover:text-secondary"
         />
       </a>
     </div>
